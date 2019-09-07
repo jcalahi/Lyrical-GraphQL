bd80452de1249da969258ed15eb8523ea09432f1
jcalahi <jcalah01@gmail.com>
2019-09-07 09:19:59 +0800

    initial commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://admin:EY2QVWdMt9qfI8pS@dev-0-ndhqp.mongodb.net/test?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
